960b0bb017f6fd1976957fccf2300ba1a32ef6da
wuguangyun <619802147@qq.com>
2017-01-12 21:20:27 +0800

    1234

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -14,8 +14,18 @@ return array(
 	//'SHOW_PAGE_TRACE' =>true,  //开启调试模式
 
 	//'配置项'=>'配置值'
+//	'DB_TYPE' => 'mysql', // 数据库类型
+//	'DB_HOST' => '127.0.0.1', // 服务器地址
+//	'DB_NAME' => 'suzhi', // 数据库名
+//	'DB_USER' => 'root', // 用户名
+//	'DB_PWD' => 'root', // 密码
+//	'DB_PORT' => 3306, // 端口
+//	'DB_PREFIX' => 'sz_', // 数据库表前缀
+//	'DB_CHARSET'=> 'utf8', // 字符集
+//	'DB_DEBUG' => TRUE,
+
 	'DB_TYPE' => 'mysql', // 数据库类型
-	'DB_HOST' => '127.0.0.1', // 服务器地址
+	'DB_HOST' => '139.224.2.99', // 服务器地址
 	'DB_NAME' => 'suzhi', // 数据库名
 	'DB_USER' => 'root', // 用户名
 	'DB_PWD' => 'root', // 密码
@@ -23,6 +33,5 @@ return array(
 	'DB_PREFIX' => 'sz_', // 数据库表前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG' => TRUE,
-
 
 );
