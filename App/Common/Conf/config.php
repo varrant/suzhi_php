16f261c0608d294d2e32c4c4b3f1eba3d1120163
root <root@localhost.localdomain>
2017-01-12 20:49:59 +0800

    test

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -15,7 +15,7 @@ return array(
 
 	//'配置项'=>'配置值'
 	'DB_TYPE' => 'mysql', // 数据库类型
-	'DB_HOST' => '127.0.0.1', // 服务器地址
+	'DB_HOST' => '139.224.2.99', // 服务器地址
 	'DB_NAME' => 'suzhi', // 数据库名
 	'DB_USER' => 'root', // 用户名
 	'DB_PWD' => 'root', // 密码
@@ -23,6 +23,6 @@ return array(
 	'DB_PREFIX' => 'sz_', // 数据库表前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG' => TRUE,
-
+    'URL_MODEL' => 0
 
 );
